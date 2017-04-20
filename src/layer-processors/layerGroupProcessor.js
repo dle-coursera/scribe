22bc234e520210a7d342ed65d3ba88ba674025bb
David Le <dle@coursera.org>
2017-04-20 10:45:06 -0700

    Improved layout detection

--- src/layer-processors/layerGroupProcessor.js
+++ src/layer-processors/layerGroupProcessor.js
@@ -27,31 +27,17 @@ export function processLayerGroup(layerGroup: MSLayerGroup): ComponentModel  {
   let parentComponent = new ComponentModel(cssModel);
   parentComponent.name = name;
 
-  let isHorizontalLayout: boolean = false;
-
-  const sortedLayers = layers.sort((first, second) => {
-    const firstFrame = first.rect();
-    const secondFrame = second.rect();
-
-    const yDiff = firstFrame.origin.y - secondFrame.origin.y;
-
-    if (yDiff === 0) {
-      isHorizontalLayout = true;
-      return firstFrame.origin.x - secondFrame.origin.x;
-    } else {
-      return yDiff;
-    }
-  });
-
-  if (isHorizontalLayout) {
-    parentComponent.cssModel.display = displayValues.flex;
-  }
-
+  let isHorizontal = isHorizontalLayout(layers);
+  const sortedLayers = sortLayers(layers);
   const layerEnumerator = sortedLayers.objectEnumerator();
 
   let lastLayer: any;
   while (layer = layerEnumerator.nextObject()) {
     let component: Component;
+
+    if (isHorizontal) {
+      parentComponent.cssModel.display = displayValues.flex;
+    }
 
     // Padding computation needs to be here, because recursivly calling this function somehow sets to currently layer to null
     let padding: Padding;
@@ -59,7 +45,7 @@ export function processLayerGroup(layerGroup: MSLayerGroup): ComponentModel  {
       const layerFrame = layer.rect();
       const lastLayerFrame = lastLayer.rect();
 
-      if (isHorizontalLayout) {
+      if (isHorizontal) {
         padding = {
           left: layerFrame.origin.x - (lastLayerFrame.origin.x + lastLayerFrame.size.width),
         }
@@ -94,4 +80,41 @@ export function processLayerGroup(layerGroup: MSLayerGroup): ComponentModel  {
   }
 
   return parentComponent;
+}
+
+// Determine the order of layers based on a linear layout
+function sortLayers(layers: Array<any>): Array<any> {
+  return layers.sort((layerA, layerB) => {
+    const frameA = layerA.rect();
+    const frameB = layerB.rect();
+
+    // Use the centroid of the layers to correct for layer overlapping.
+    const centerAx = frameA.origin.x + frameA.size.width/2;
+    const centerAy = frameA.origin.y + frameA.size.height/2;
+
+    const centerBx = frameB.origin.x + frameB.size.width/2;
+    const centerBy = frameB.origin.y + frameB.size.height/2;
+
+    const xDiff = centerAx - centerBx;
+    const yDiff = centerBy - centerBy;
+
+    if (xDiff < 0 || yDiff < 0) {
+      return -1; // Layer A comes before layer B
+    } else if (xDiff > 0 || yDiff > 0) {
+      return 1; // Layer B comes before Layer A
+    } else {
+      return 0; // Order is not deterministic
+    }
+  });
+}
+
+function isHorizontalLayout(layers: Array<any>): boolean {
+  if (layers.length > 1) {
+    const frameA = layers[0].rect();
+    const frameB = layers[1].rect();
+    // Check if the right edge of layer A is less than the left edge of layer B offset by 20% of the width
+    return (frameA.origin.x + frameA.size.width) < (frameB.origin.x + frameB.size.width * 0.2);
+  } else {
+    return false;
+  }
 }
